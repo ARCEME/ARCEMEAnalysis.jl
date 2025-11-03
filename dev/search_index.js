55ac4eb0f03d520ec2c06ae49456312f1c22f93d
Documenter.jl <documenter@juliadocs.github.io>
2025-11-03 16:06:53 +0000

    build based on 5b5507c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ARCEMEAnalysis","page":"Home","title":"ARCEMEAnalysis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ARCEMEAnalysis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
