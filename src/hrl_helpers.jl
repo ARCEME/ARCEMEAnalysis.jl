85a060b2b8214c016ef5c2c12180265c62237e1e
Mélanie Weynants <45990429+melwey@users.noreply.github.com>
2026-03-06 09:56:30 +0000

    hrl_warp

--- src/hrl_helpers.jl
+++ src/hrl_helpers.jl
@@ -1,8 +1,13 @@
 module HRL
+using ARCEMEAnalysis
 import CondaPkg
 using  PythonCall: pyimport, pyconvert, pylist
 import GeoJSON
 using Preferences: @set_preferences!, @load_preference
+import ArchGDAL as AG
+using YAXArrays
+using Dates
+using ProgressMeter
 
 """
 hrl_set_localpath(path)
@@ -85,13 +90,13 @@ function  hda_years(dataset_id)
 end
 
 
-dataset_id = "EO:EEA:DAT:HRL:CRL";yr="2023";coord=(-1.0, 38.6);product="Crop Types";extent=0.05
 """
-`hrl_download(dataset_id::String, coordinates::Tuple, yr::Union{String,Int}; extent=0.05, hrl_localpath=hrl_localpath, product::{Union{Nothing,String}}=nothing)`
+`hrl_download(dataset_id::String, coordinates::Tuple, yr::Union{String,Int}; extent=0.05, product::{Union{Nothing,String}}=nothing)`
+`hrl_download(dataset_id::Vector{String}, coordinates::Tuple, yr::Union{String,Int}; extent=0.05, product::{Union{Nothing,String}}=nothing)`
 
 Downloads Copernicus Land Monitoring Service High Resolution Layer products from WEkEO using the Harmonised Data Access API. 
-  `dataset_id` must be a valid dataset (e.g., Cropland: "EO:EEA:DAT:HRL:CRL"; Tree cover and Forest: "EO:EEA:DAT:HRL:TCF"; 
-  Grassland: "EO:EEA:DAT:HRL:GRA").
+  `dataset_id` must be a single valid dataset (e.g., Cropland: "EO:EEA:DAT:HRL:CRL"; Tree cover and Forest: "EO:EEA:DAT:HRL:TCF"; 
+  Grassland: "EO:EEA:DAT:HRL:GRA") or a vector of vallid datasets.
   Search for all tiles near `coordinates` with (lon, lat) +- `extent`, for year `yr`. Some products are only available for three-years epochs.
   Data are downloaded at `hrl_localpath` only if they do not already exist.
   By default, all products from `dataset_id` are returned. To search for a specific product, set `product` to a valid productType (e.g. "Crop Types")
@@ -100,7 +105,7 @@ Returns a Vector of filenames.
 
 See also `hda_datasets`, `hda_products`, `hda_years`
 """
-function hrl_download(dataset_id::String, coord::Tuple, yr::Union{String,Int}; extent=0.05, hrl_localpath=hrl_localpath, product::Union{Nothing,String}=nothing)
+function hrl_download(dataset_id::String, coord::Tuple, yr::Union{String,Int}; extent=0.05, product::Union{Nothing,String}=nothing)
     hda_client = hrl_hda()
     json = pyimport("json")
 
@@ -147,12 +152,344 @@ function hrl_download(dataset_id::String, coord::Tuple, yr::Union{String,Int}; e
 
     # Download
     todownload = [!isfile(joinpath(hrl_localpath, pyconvert(String, item["id"])*".zip")) for  item in matches.results]
-    for i in pylist(findall(todownload).-1)
-      println("Downloading $(matches[i]["id"])")
+    @showprogress desc="Downloading..." for i in pylist(findall(todownload).-1)
+    #   println("Downloading $(matches.results[i]["id"])")
       matches[i].download(download_dir=hrl_localpath) 
     end
     return fnames
 end
 
-export hrl_download, hrl_set_localpath, hda_datasets, hda_products, hda_years
+function hrl_download(datasets::Vector{String}, coord::Tuple, yr::Union{String,Int}; kwargs...)
+  fnames=[];
+  for dataset_id in datasets
+      append!(fnames, HRL.hrl_download(dataset_id, coord, yr; kwargs...))
+  end
+  return fnames
+end
+
+hrl_dict = Dict(
+    "CTY" => "Crop types",
+    "CTYCL" => "Crop types confidence level",
+    "CPMCH" => "Main Crop Harvest",
+    "CPMCHCL" => "Main Crop Harvest confidence level",
+    "CPMCE " => "Main Crop Emergence",
+    "CPMCECL" => "Main Crop Emergence confidence level",
+    "CPMCD" => "Main Crop Duration",
+    "CPMCDCL" => "Main Crop Duration confidence level",
+    "CPBSB" => "Bare soil before",
+    "CPBSBCL" => "Bare soil before confidence level",
+    "CPBSA" => "Bare soil after",
+    "CPBSACL" => "Bare soil after confidence level",
+    "CPSCT" => "Secondary Crop Type",
+    "CPSCE" => "Secondary Crop Emergence",
+    "CPSCD" => "Secondary Crop Duration",
+    "CPSCDCL" => "Secondary Crop Duration confidence level",
+    "CPFLP" => "Fallow Land Presence",
+    "CPFLPCL" => "Fallow Land Presence confidence level",
+    # "CPFLD" => "Fallow Land Duration",
+    # "CPFLDCL" => "Fallow Land Duration confidence level",
+    "CPCSY" => "Cropping Seasons Yearly",
+    # Forest
+    "DLT" => "Dominant Leaf Type",
+    "DLTCL" => "Dominant Leaf Type Confidence Layer",
+    "TCD" => "Tree Cover Density",
+    "TCDCL" => "Tree Cover Density Confidence Layer",
+    # Grassland
+    "GRA" => "Grassland",
+    "GRACL" => "Grassland Confidence Layer",
+    "HER" => "Herbaceous Cover",
+    "PLOUGH" => "Ploughing Indicator",
+    "GRAME"  => "Grassland Mowing Events",
+    "GRAMECL" => "Grassland Mowing Events Confidence Layer",
+    "GRAMD1" => "Grassland Mowing Dates of first mowing event",
+    "GRAMD2" => "Grassland Mowing Dates of second mowing event",
+    "GRAMD3" => "Grassland Mowing Dates of third mowing event",
+    "GRAMD4" => "Grassland Mowing Dates of fourth mowing event",
+)
+
+hrl_legends = Dict(
+    "CTY" => Dict(
+        0 =>  "No cropland",
+        1110 => "Wheat",
+        1120 => "Barley",
+        1130 => "Maize",
+        1140 => "Rice",
+        1150 => "Other cereals",
+        1210 => "Fresh Vegetables",
+        1220 => "Dry Pulses",
+        1310 => "Potatoes",
+        1320 => "Sugar Beet",
+        1410 => "Sunflower",
+        1420 => "Soybeans",
+        1430 => "Rapeseed",
+        1440 => "Flax cotton and hemp",
+        2100 => "Grapes",
+        2200 => "Olives",
+        2310 => "Fruits",
+        2320 => "Nuts",
+        3100 => "Unclassified annual crop",
+        3200 => "Unclassified permanent crop",
+        65535 => "outside area"
+    ),
+    "CTYCL" => "0-100 Probability expressed as a percentage. 253 No Cropland. 255 Outside area",
+    "CPMCH" => "Harvest date as YYDOY where YY = last 2 digits of the year (e.g., 19 for 2019) and DOY is the day of the year (1-366). 
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        65535 Outside area.",
+    "CPMCHCL" => "Uncertainty in days (1-40). 
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        64534 No confidence could be calculated.
+        65535 Outside area.",   
+    "CPMCE" => "Emergence date as YYDOY where YY = last 2 digits of the year (e.g., 19 for 2019) and DOY is the day of the year (1-366). 
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        65535 Outside area.",
+    "CPMCECL" => "Uncertainty in days (1-40). 
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        64534 No confidence could be calculated.
+        65535 Outside area.",
+    "CPMCD" => "Days (40-366).  
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        65535 Outside area.",
+    "CPMCDCL" => "Uncertainty in days (1-80). 
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        64534 No confidence could be calculated.
+        65535 Outside area.",
+    "CPBSB" => "Days (1-295).  
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65529 Period outside calendar boundaries. 
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        65535 Outside area.",
+    "CPBSBCL" => "Uncertainty in days (1-80). 
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65529 Period outside calendar boundaries. 
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        64534 No confidence could be calculated.
+        65535 Outside area.",
+    "CPBSA" => "Days (1-295).  
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65529 Period outside calendar boundaries. 
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        65535 Outside area.",
+    "CPBSACL" => "Uncertainty in days (1-80). 
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65529 Period outside calendar boundaries. 
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        64534 No confidence could be calculated.
+        65535 Outside area.",
+    "CPSCT" => Dict(
+        1 => "Short Summer",
+        2 => "Long Summer",
+        3 => "Short Winter",
+        4 => "Long Winter", 
+        0 => "No annual cropland", 
+        65526 => "Fallow land", 
+        65527 => "No cropping pattern detected",  
+        65530 => "No secondary crop growing season delineated",
+        65531 => "Not enough data", 
+        65532 => "No cropping season detected",  
+        65533 => "Growing season extends beyond timeframe",
+        65535 => "Outside area"
+    ),
+    "CPSCE" => "Emergence date as YYDOY where YY = last 2 digits of the year (e.g., 19 for 2019) and DOY is the day of the year (1-366). 
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65530 No secondary crop growing season delineated.
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        65535 Outside area.",
+    "CPSCD" => "Days (40-366).  
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65530 No secondary crop growing season delineated.
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        65535 Outside area.",
+    "CPSCDCL" => "Uncertainty in days (1-80). 
+        0 No annual cropland. 
+        65526 Fallow land. 
+        65527 No cropping pattern detected. 
+        65530 No secondary crop growing season delineated.
+        65531 Not enough data. 
+        65532 No cropping season detected. 
+        65533 Growing season extends beyond timeframe. 
+        64534 No confidence could be calculated.
+        65535 Outside area.",
+    "CPFLP" => Dict(
+        1 => "Fallow land",
+        0 => "No annual cropland",
+        65535 => "Outside area"
+    ),
+    "CPFLPCL" => "0-100 Probability expressed as a percentage. 253 No Cropland. 65535 Outside area",
+    "CPCSY" => Dict(
+        1 => "One growing season",
+        2 => "Two growing seasons",
+        0 => "No annual cropland", 
+        65526 => "Fallow land", 
+        65527 => "No cropping pattern detected",  
+        65531 => "Not enough data", 
+        65532 => "No cropping season detected",  
+        65533 => "Growing season extends beyond timeframe",
+        65535 => "Outside area"
+    ),
+    # Forest
+    "DLT" => Dict(
+        0 => "all non-tree covered areas",
+        1 => "broadleaved trees",
+        2 => "coniferous trees",
+        255 => "outside area",
+    ),
+    "DLTCL" => "0-100: classification confidence. 
+            253: all non-tree covered areas. 
+            255: outside area",
+    "TCD" => "0: all non-tree covered areas. 
+            1-100: tree cover density in %. 
+            255: outside area",
+    "TCDCL" => "0-100: standard deviation of TCD estimate. 
+            253: all non-tree covered areas
+            255: outside area",
+    # Grassland
+    "GRA" => Dict(
+        0 => "all non-grassland areas",
+        1 => "grassland", 
+        255 => "outside area",
+    ),
+    "GRACL" => "0-100: Classification confidence. 253: All non-grassland areas. 255: outside area",
+    "HER" => Dict(
+        0 => "non-grassland in reference year",
+        1 => "temporary grassland in reference year",
+        255 => "outside area",
+    ),
+    "PLOUGH" => "0: Indication of ploughing in current year. 
+    1-6: Number of years since last indication of ploughing. 
+    100: Change in herbaceous cover. 
+    253: no ploughing information. 
+    255: outside area",
+    "GRAME"  => Dict(
+        0 => "no mowing detected", 
+        1-4 => "Number of mowing events detected",
+        253 => "all non-herbaceous areas",
+        255 => "outside area"
+    ),
+    "GRAMECL" => "0-100: Mowing detection confidence. 253: All non-mowing areas. 255: outside area",
+    "GRAMD1" => "0: no mowing detected. 1-366: Start (DOY) of each mowing event. 65533: all non-herbaceous areas. 65535: outside area",
+    "GRAMD2" => "0: no mowing detected. 1-366: Start (DOY) of each mowing event. 65533: all non-herbaceous areas. 65535: outside area",
+    "GRAMD3" => "0: no mowing detected. 1-366: Start (DOY) of each mowing event. 65533: all non-herbaceous areas. 65535: outside area",
+    "GRAMD4" => "0: no mowing detected. 1-366: Start (DOY) of each mowing event. 65533: all non-herbaceous areas. 65535: outside area",
+)
+
+"""
+`hrl_warp(event::ARCEMEAnalysis.Event; batch="ARCEME-DC-6", dataset_id::Union{String, Vector{String}}=["EO:EEA:DAT:HRL:CRL", "EO:EEA:DAT:HRL:TCF", "EO:EEA:DAT:HRL:GRA"])`
+`hrl_warp(cubename, fnames::Vector{String}; batch="ARCEME-DC-6")`
+`hrl_warp(cubename::String; batch="ARCEME-DC-6", dataset_id::Union{String, Vector{String}}=["EO:EEA:DAT:HRL:CRL", "EO:EEA:DAT:HRL:TCF", "EO:EEA:DAT:HRL:GRA"])`
+
+reprojects and clips files in `fnames` to match `event`'s coordinate reference system and extent,
+ and saves them as a Zarr group in `batch-HRL` under `ARCEMEAnalysis.local_cubepath`.
+"""
+function hrl_warp(cubename, fnames::Vector; batch="ARCEME-DC-6")
+  ds = arceme_open(cubename; batch)
+  event_date = event_date = arceme_eventdate(ds)
+  tmp = tempname(;suffix=".tif")
+  res = ds.properties["resolution"]
+  tmp = tempname(;suffix=".tif")
+
+  for hrl in keys(hrl_dict)
+    hrl_fnames = fnames[map(contains("$(hrl)_"), fnames)]
+    if isempty(hrl_fnames)
+        continue
+    end
+    run(Cmd(
+        `gdalwarp 
+            -t_srs $(AG.toPROJ4(AG.importEPSG(ds.properties["epsg"]))) 
+            -tr $res $res
+            -te $(minimum(ds.x)) $(minimum(ds.y)) $(maximum(ds.x)+res) $(maximum(ds.y)+res) 
+            -r near
+            $(["/vsizip/$(joinpath(hrl_localpath, hrl_fnames[i], "$(split(hrl_fnames[i],".")[1]).tif") )" for i in eachindex(hrl_fnames)]) 
+            $tmp 
+            -overwrite 
+            `))
+    c = Cube(tmp)
+    # write to zarr
+    props = Dict(
+        "long_name" => hrl_dict["$(split(hrl_fnames[1],"_")[3])"],
+        "year_coverage" => year(event_date),
+        "legend" => hrl_legends["$(split(hrl_fnames[1],"_")[3])"],
+        "epsg" => ds.properties["epsg"], 
+        "resolution" => 10,
+        "unit" => "meter",
+        "source" => "$(hrl_fnames[1]) © European Union, Copernicus Land Monitoring Service 2025, European Environment Agency (EEA)",
+        "processing" => "Downloaded from wEkEO. Reprojected to EPSG $(ds.properties["epsg"]) using nearest neighour and clipped to AOI with gdalwarp",
+        "processed_by" => "M. Weynants, MPI-BGC",
+        "processed_date" => string(Date(now())),
+    )
+    c2 = YAXArray((ds.x,ds.y), c.data, props, )
+    nt = (Symbol("$(split(hrl_fnames[1],"_")[3])")=>c2,)
+    tds = Dataset(;nt...)
+    tds = setchunks(tds, (500,500))
+    savedataset(tds, 
+        path=joinpath(ARCEMEAnalysis.local_cubepath, "$(batch)-HRL", "$(split(cubename,".")[1]).zarr"), 
+        append=true)
+    end
+    return joinpath(ARCEMEAnalysis.local_cubepath, "$(batch)-HRL", "$(split(cubename,".")[1]).zarr")
+end
+
+hrl_warp(event::ARCEMEAnalysis.Event; kwargs...) = hrl_warp(arceme_cubename(event); kwargs...)
+
+function hrl_warp(cubename::String; batch="ARCEME-DC-6", dataset_id::Union{String, Vector{String}}=["EO:EEA:DAT:HRL:CRL", "EO:EEA:DAT:HRL:TCF", "EO:EEA:DAT:HRL:GRA"])
+  ds = arceme_open(cubename; batch=batch)
+  coord = arceme_coordinates(ds)
+  yr = year(arceme_eventdate(ds))
+  fnames = hrl_download(dataset_id, coord, yr)
+  hrl_warp(cubename, fnames; batch)
+end
+
+export hrl_download, hrl_set_localpath, hda_datasets, hda_products, hda_years, hrl_warp
 end #module
