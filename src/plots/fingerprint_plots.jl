06d7c7865940509a1efe6913b5097d672cd0fd19
Fabian Gans <fgans@bgc-jena.mpg.de>
2026-03-24 15:30:57 +0000

    Updates to script

--- src/plots/fingerprint_plots.jl
+++ src/plots/fingerprint_plots.jl
@@ -1,82 +1,50 @@
 module FingerprintPlots
-using Colors:RGBA
+using Colors: RGBA
 using Makie
 using Dierckx
 using YAXArrays
 using Dates
 using ..ARCEMEAnalysis
-using ..ARCEMEAnalysis: local_cubepath
-using DataStructures: counter
+using ..ARCEMEAnalysis: local_cubepath, arceme_classes
+using DataStructures: counter, OrderedDict
 using Statistics: mean
 using GeoMakie
 import DimensionalData as DD
 
-function filter_spline(xout, samplets,x,nout)
-    missmask = findall(x->!ismissing(x) && isfinite(x),samplets)
+function filter_spline(xout, samplets, x, nout)
+    missmask = findall(x -> !ismissing(x) && isfinite(x), samplets)
     isempty(missmask) && return xout .= missing
     xgood = x[missmask]
     ygood = samplets[missmask]
-    
+
     spl = nothing
     nknots = 19
 
-    kn = compute_knots(xgood,nknots)
+    kn = compute_knots(xgood, nknots)
 
     spl = try
-        Spline1D(xgood,ygood,kn)
+        Spline1D(xgood, ygood, kn)
     catch
         return xout .= NaN
     end
 
-    xout .= evaluate(spl,range(0,2,length=nout))
+    xout .= evaluate(spl, range(0, 2, length=nout))
 end
 
 
-function arceme_representative_image(ev; index_use="kNDVI", brighten_factor=2, lc = nothing)
-    ds = arceme_open(ev)
-    fp = arceme_open_fingerprint(ev)
-    sumcl = xmap(ds.cloud_mask ⊘ (:x,:y),ds.SCL ⊘ (:x,:y),inplace=false,output=XOutput(outtype=Float64)) do cl,scl
-        sum(i->ARCEMEAnalysis._is_cloud(i...),zip(cl,scl))/length(cl)
-    end
-    sumcl = sumcl[1,1,:]
-    alllc = if lc === nothing 
-        xmap(fp.s2_indices[band=DD.At(index_use)] ⊘ :lc, inplace=false) do vals
-            all(ismissing,vals) && return NaN
-            mean(skipmissing(vals))
-        end
-    else
-        fp.s2_indices[band=DD.At(index_use),lc=DD.At(lc)]
-    end
-    alllcranks = sortperm(alllc.data[:],rev=true)
-    repr_ind = 0
-    for i in 1:length(alllcranks)
-        icandidate = findfirst(==(i),alllcranks)
-        if sumcl.data[icandidate]< 0.05
-            repr_ind = icandidate
-            break
-        end
-    end
-    brfilt(a,factor,alpha) = RGBA(clamp(a.r*factor,0,1),clamp(a.g*factor,0,1),clamp(a.b*factor,0,1),alpha)
-    ilc = findfirst(==(lc),collect(values(arceme_classes)))
-    r = broadcast(arceme_rgb(ds)[time_sentinel_2_l2a=repr_ind],ds.ESA_LC[:,:,1],brighten_factor) do col,lc,f
-        lcv = ARCEMEAnalysis.lckeymap(lc)
-        alpha = lcv == ilc ? 1.0 : 0.5
-        brfilt(col,f,alpha)
-    end
-    r[:,:]
-end
+
 
 pairnow = arceme_validpairs()[4]
 
 
 
 function most_common_lc(pairnow)
-    c1,c2 = map(pairnow) do ev
+    c1, c2 = map(pairnow) do ev
         ds = arceme_open(ev)
-        counter(ds.ESA_LC.data[:,:,1])
+        counter(ds.ESA_LC.data[:, :, 1])
     end
     #Find maximum landcover in both cubes
-    (_,i1),(_,i2) = findmax.((c1,c2))
+    (_, i1), (_, i2) = findmax.((c1, c2))
     if c1[i1] + c2[i1] > c1[i2] + c2[i2]
         arceme_classes[i1]
     else
@@ -84,85 +52,142 @@ function most_common_lc(pairnow)
     end
 end
 
+function pairwise_without_missings(x, y)
+    out = Point2f[]
+    for i in eachindex(x, y)
+        if !ismissing(y[i]) && isfinite(y[i])
+            push!(out, Point2f(x[i], y[i]))
+        end
+    end
+    out
+end
 
+function open_plot_data(ev, lc)
+    @info "Opening $ev"
+    ds = arceme_open_fingerprint(ev)
+    firstday = arceme_eventdate(ev) - Year(1)
+    lastday = arceme_eventdate(ev) + Year(1)
+    nms = Millisecond(lastday - firstday).value
+    linscal = d -> Millisecond(d - firstday).value * 2 / nms
+    x_s2 = map(linscal, ds.time_sentinel_2_l2a.val)
+    x_s1 = map(linscal, ds.time_sentinel_1_rtc.val)
+    outax = YAXArrays.time(range(firstday, lastday, step=Day(1)))
 
-function fingerprint_plot(pairnow; lc=most_common_lc(pairnow))
-    lc_choos = lc
-    d,dhp = map(pairnow) do ev
-        ds = arceme_open_fingerprint(ev)
-        firstday = arceme_eventdate(ev) - Year(1)
-        lastday = arceme_eventdate(ev) + Year(1)
-        nms = Millisecond(lastday-firstday).value
-        linscal = d->Millisecond(d-firstday).value*2/nms
-        x_s2 = map(linscal,ds.time_sentinel_2_l2a.val)
-        x_s1 = map(linscal,ds.time_sentinel_1_rtc.val)
-        outax = YAXArrays.time(range(firstday,lastday,step=Day(1)))
+    s2 = ds.s2_indices[lc=DD.At(lc)]
+    s1 = ds.s1_indices[lc=DD.At(lc)]
 
-        r_s2 = xmap(filter_spline,ds.s2_indices ⊘ :time_sentinel_2_l2a, output=XOutput(outax), function_args=(x_s2, length(outax),))
-        r_s1 = xmap(filter_spline,ds.s1_indices ⊘ :time_sentinel_1_rtc, output=XOutput(outax), function_args=(x_s1, length(outax),))
+    r_s2 = xmap(filter_spline, s2 ⊘ :time_sentinel_2_l2a, output=XOutput(outax), function_args=(x_s2, length(outax),))
+    r_s1 = xmap(filter_spline, s1 ⊘ :time_sentinel_1_rtc, output=XOutput(outax), function_args=(x_s1, length(outax),))
 
-        ds, r_s1, r_s2, x_s1, x_s2
+    dataplots = OrderedDict()
+    interplots = OrderedDict()
+
+    for band in ds.band_s2
+
+        interplots[band] = pairwise_without_missings(range(0, 2, length=length(outax)), r_s2[band_s2=DD.At(band)].data[:])
+        dataplots[band] = pairwise_without_missings(x_s2, s2[band_s2=DD.At(band)].data[:])
     end
 
-    fig = Figure(size=(1600,2600),title=lc_choos);
+    for band in ds.band_s1
+        interplots[band] = pairwise_without_missings(range(0, 2, length=length(outax)), r_s1[band_s1=DD.At(band)].data[:])
+        dataplots[band] = pairwise_without_missings(x_s1, s1[band_s1=DD.At(band)].data[:])
+    end
+
+    (dataplots, interplots)
+end
+
+
+function fingerprint_plot(pairid=1; lc=nothing, plotdict=IdDict())
+
+    ipair = Observable{Int}(pairid)
+    alleventpairs = arceme_validpairs()
+
+    if lc === nothing
+        lc = most_common_lc(alleventpairs[pairid])
+    end
+
+    lc_choos = Observable{Any}(lc)
+
+    d = lift(ipair, lc_choos) do i, lc
+        ev = alleventpairs[i][1]
+        open_plot_data(ev, lc)
+    end
+    dhp = lift(ipair, lc_choos) do i, lc
+        ev = alleventpairs[i][2]
+        open_plot_data(ev, lc)
+    end
+    fig = Figure(size=(1600, 2600))
+
+    allbands = collect(keys(d[][1]))
 
     allax = Dict()
 
-    for (i_d,(ds,r_s1,r_s2,x_s1,x_s2)) in enumerate((d,dhp))
-        colcur = (:red,:blue)[i_d]
-        x,r = x_s2,r_s2
-        for (i,index_choos) in enumerate(ds.band_s2.val)
-            orig = ds.s2_indices[lc=DD.At(lc_choos),band_s2=DD.At(index_choos)].data[:]
-            x_orig = x[(!ismissing).(orig)]
-            ax = Axis(fig[i,i_d],title=index_choos)
-            allax[(i,i_d)] = ax 
-            lines!(ax,range(0,2,length(r.time)),r[lc=DD.At(lc_choos),band_s2=DD.At(index_choos)].data[:],color=colcur);
-            plot!(ax,x_orig,orig[(!ismissing).(orig)],color=colcur)
-        end
-        x,r = x_s1,r_s1
-
-        for (i,index_choos) in enumerate(ds.band_s1.val)
-            orig = ds.s1_indices[lc=DD.At(lc_choos),band_s1=DD.At(index_choos)].data[:]
-            x_orig = x[(!ismissing).(orig)]
-            ax = Axis(fig[i+length(ds.band_s2),i_d],title=index_choos) 
-            allax[(i+length(ds.band_s2),i_d)] = ax
-            lines!(ax,range(0,2,length(r.time)),r[lc=DD.At(lc_choos),band_s1=DD.At(index_choos)].data[:],color=colcur);
-            plot!(ax,x_orig,orig[(!ismissing).(orig)],color=colcur)
+    for (i_d, o) in enumerate((d, dhp))
+        colcur = (:red, :blue)[i_d]
+        dataplots = lift(first, o)
+        interplots = lift(last, o)
+        i_pl = 1
+        for index_choos in allbands
+            ax = Axis(fig[i_pl, i_d], title=index_choos)
+            allax[(i_pl, i_d)] = ax
+            data_interp = lift(i -> i[index_choos], interplots)
+            data_orig = lift(i -> i[index_choos], dataplots)
+            lines!(ax, data_interp, color=colcur)
+            p = plot!(ax, data_orig, color=colcur)
+            plotdict[p] = (index_choos, i_d)
+            i_pl += 1
         end
     end
 
-    npl = maximum(first,keys(allax))
+    npl = maximum(first, keys(allax))
     for ipl in 1:npl
-        linkyaxes!(allax[(ipl,1)],allax[(ipl,2)])
+        linkyaxes!(allax[(ipl, 1)], allax[(ipl, 2)])
     end
-    ax = Axis(fig[4:7,3],aspect=DataAspect(),title="Drought")
-    heatmap!(ax,arceme_representative_image(pairnow[1],lc=lc_choos))
-    ax = Axis(fig[8:11,3],aspect=DataAspect(),title="Drought + HP")
-    heatmap!(ax,arceme_representative_image(pairnow[2],lc=lc_choos))
+    ax = Axis(fig[5:6, 3], aspect=DataAspect(), title="Drought")
+    img1 = lift((y, i) -> arceme_representative_image(alleventpairs[i][1], lc=y), lc_choos, ipair)
+    heatmap!(ax, img1)
+    ax = Axis(fig[7:8, 3], aspect=DataAspect(), title="Drought + HP")
+    img2 = lift((y, i) -> arceme_representative_image(alleventpairs[i][2], lc=y), lc_choos, ipair)
+    heatmap!(ax, img2)
 
-    x = [pairnow[1].longitude, pairnow[2].longitude]
-    y = [pairnow[1].latitude, pairnow[2].latitude]
-    img = YAXArray((lon(-179.75:0.5:179.75),lat(-89.75:0.5:89.75)),rotr90(GeoMakie.earth()));
-    gax = GeoAxis(fig[1:3,3],source="+proj=longlat",dest = "+proj=longlat")
-    heatmap!(gax, img; interpolate = false)
-    scatter!(gax,x,y,color=[:red,:blue])
-    xlims!(gax,minimum(x)-20,maximum(x)+20)
-    ylims!(gax,minimum(y)-20,maximum(y)+20)
+
+    xpos = lift(i -> [alleventpairs[i][1].longitude, alleventpairs[i][2].longitude], ipair)
+    ypos = lift(i -> [alleventpairs[i][1].latitude, alleventpairs[i][2].latitude], ipair)
+    img = YAXArray((lon(-179.75:0.5:179.75), lat(-89.75:0.5:89.75)), rotr90(GeoMakie.earth()))
+    gax = GeoAxis(fig[3:4, 3], source="+proj=longlat", dest="+proj=longlat")
+    heatmap!(gax, img; interpolate=false)
+    scatter!(gax, xpos, ypos, color=[:red, :blue])
+    # xlims!(gax, lift(xpos -> minimum(xpos) - 20, xpos), lift(xpos -> maximum(xpos) + 20, xpos))
+    # ylims!(gax, lift(ypos -> minimum(ypos) - 20, ypos), lift(ypos -> maximum(ypos) + 20, ypos))
+    menupair = Slider(fig, range=1:length(alleventpairs), startvalue=40, update_while_dragging=false)
+    menulc = Menu(fig, options=collect(values(arceme_classes)), default=lc)
+    fig[1:2, 3] = vgrid!(
+        Label(fig, "Event Pair", width=nothing),
+        menupair,
+        Label(fig, "Land Cover", width=nothing),
+        menulc,
+    )
+    on(menulc.selection) do s
+        lc_choos[] = s
+    end
+    on(menupair.value) do v
+        ipair[] = v
+    end
     fig
 end
 
-function compute_knots(xgood,ntarget=19)
-    xtarget = range(0.0,2.0,length=ntarget)
-    kn = filter(x->first(xgood) < x < last(xgood), xtarget)
+function compute_knots(xgood, ntarget=19)
+    xtarget = range(0.0, 2.0, length=ntarget)
+    kn = filter(x -> first(xgood) < x < last(xgood), xtarget)
     knotstoreplace = []
     for i in 1:(length(kn)-1)
-        ix = searchsortedfirst(xgood,kn[i])
-        ix2 = searchsortedfirst(xgood,kn[i+1])
-        if ix2-ix <= 1
+        ix = searchsortedfirst(xgood, kn[i])
+        ix2 = searchsortedfirst(xgood, kn[i+1])
+        if ix2 - ix <= 1
             if !isempty(knotstoreplace) && last(last(knotstoreplace)) == i
-                push!(last(knotstoreplace), i+1)
+                push!(last(knotstoreplace), i + 1)
             else
-                push!(knotstoreplace,[i,i+1])
+                push!(knotstoreplace, [i, i + 1])
             end
         end
     end
@@ -170,9 +195,9 @@ function compute_knots(xgood,ntarget=19)
         return kn
     else
         newknots = map(knotstoreplace) do group
-            mean(view(kn,group))
+            mean(view(kn, group))
         end
-        return sort([kn[setdiff(1:length(kn),reduce(vcat,knotstoreplace))];newknots])
+        return sort([kn[setdiff(1:length(kn), reduce(vcat, knotstoreplace))]; newknots])
     end
 end
 
